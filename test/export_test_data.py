e2a065bd40375fd062a4839dd51c43e6d2835d7f
cmaumet <c.m.j.maumet@warwick.ac.uk>
2015-11-24 10:32:21 +0000

    debug print msg

--- test/export_test_data.py
+++ test/export_test_data.py
@@ -50,9 +50,19 @@ class ExportTestData(object):
             origin = data_repo.remote("origin")
             origin.pull()
 
-        # Not sure why this is needed on Travis CI to get the files
+        # Not sure why this is needed on Travis CI to get the files 
         # (instead of pointers)
-        check_call("cd " + test_data_dir + "; git checkout .", shell=True)
+        check_call("cd "+test_data_dir+ "; git checkout .", shell=True)
+
+        print "\n\n\n --------------------"
+        check_call("cd "+test_data_dir+ "; git lfs ls-files", shell=True)
+        print "\n\n\n --------------------"        
+            
+        print "\n\n\n --------------------"        
+        print "cat "+test_data_dir+ "/fsl_voxelwise_p0001/stats/sigmasquareds.nii.gz"
+        # check_call("cat "+test_data_dir+ "/fsl_voxelwise_p0001/design.png", shell=True)
+        check_call("cat "+test_data_dir+ "/fsl_voxelwise_p0001/stats/sigmasquareds.nii.gz", shell=True)
+        print "\n\n\n --------------------"
 
         # Find all test data to be compared with ground truth
         # test_files = glob.glob(os.path.join(TEST_DATA_DIR, '*', '*.ttl'))
